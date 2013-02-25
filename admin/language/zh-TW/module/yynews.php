e5ffffccd75faac37d920d33342dcf7a02251482
u18-1997 <u18_1997@163.com>
2013-02-25 20:09:52 +0800

    增加了台湾繁体语言包

---
+++ admin/language/zh-TW/module/yynews.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']       = '新聞信息';
+
+// Text
+$_['text_module']         = '模塊管理';
+$_['text_success']        = '成功： 您已修改信息模塊！';
+$_['text_content_top']    = '內容的上面';
+$_['text_content_bottom'] = '內容的底部';
+$_['text_column_left']    = '左列顯示';
+$_['text_column_right']   = '右列顯示';
+$_['text_column_maxrows']   = '顯示最大行數';
+$_['text_column_displayimg']   = '是否顯示標題圖片';
+
+// Entry
+$_['entry_layout']        = '佈局：';
+$_['entry_position']      = '顯示位置：';
+$_['entry_status']        = '狀態：';
+$_['entry_sort_order']    = '排序：';
+
+// Error
+$_['error_permission']    = '警告： 您沒有權限變更信息模塊！';
+?>
