5849069ee3264517e6afc0303c5c1e2069608453
u18-1997 <u18_1997@163.com>
2013-03-01 10:48:25 +0800

    数据库添加前缀

--- yynews.sql
+++ yynews.sql
@@ -1,16 +1,11 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.28-0ubuntu0.12.04.3 - (Ubuntu)
--- Server OS:                    debian-linux-gnu
--- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2013-02-17 12:49:19
+-- yynews数据库脚本
+-- 
+--注意：如果您数据库中的表添加了前缀，请自行在新添加的这两个表的表名上添加前缀
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
 
--- Dumping structure for table opencart.yynews
+
 CREATE TABLE IF NOT EXISTS `yynews` (
   `yynews_id` int(11) NOT NULL AUTO_INCREMENT,
   `top` int(1) NOT NULL DEFAULT '0',
@@ -21,10 +16,8 @@ CREATE TABLE IF NOT EXISTS `yynews` (
   PRIMARY KEY (`yynews_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Data exporting was unselected.
 
 
--- Dumping structure for table opencart.yynews_description
 CREATE TABLE IF NOT EXISTS `yynews_description` (
   `yynews_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL,
@@ -33,7 +26,3 @@ CREATE TABLE IF NOT EXISTS `yynews_description` (
   `summary` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`yynews_id`,`language_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- Data exporting was unselected.
-/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
